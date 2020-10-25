d6ca93b8e11274c5cdc281a598554420d2ae57d1
Mora Ogunro <18519716+MoraOgunro@users.noreply.github.com>
2020-10-24 20:13:51 -0500

    BaccaratDealer done

--- src/main/java/BaccaratDealer.java
+++ src/main/java/BaccaratDealer.java
@@ -1,11 +1,12 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Random;
 
 public class BaccaratDealer {
     ArrayList<Card> deck = new ArrayList<>();
 
     public void generateDeck(){
-        //TODO generate a new standard 52 card deck where each card is an instance of the Card class in the ArrayList<Card> deck
         deck.clear();
         generateDeckHelper("Clubs");
         generateDeckHelper("Diamonds");
@@ -13,20 +14,28 @@ public class BaccaratDealer {
         generateDeckHelper("Spades");
     }
     public ArrayList<Card> dealHand(){
-        //TODO deal two cards and return them in an ArrayList<Card>.
-        return null;
+        ArrayList<Card> c = new ArrayList<>();
+        c.add(drawOne());
+        c.add(drawOne());
+        return c;
     }
     public Card drawOne(){
-        //TODO deal a single card and return it.
-
-        return null;
+        Random random = new Random();
+        int index = random.nextInt(deck.size());
+        Card card = deck.get(index);
+        deck.remove(index);
+        return card;
     }
     public void shuffleDeck(){
-        //TODO create a new deck of 52 cards and “shuffle”
+        deck.clear();
+        generateDeckHelper("Clubs");
+        generateDeckHelper("Diamonds");
+        generateDeckHelper("Hearts");
+        generateDeckHelper("Spades");
+        Collections.shuffle(deck);
     }
     public int deckSize(){
-        //TODO return how many cards are in this.deck at any given time
-        return 0;
+        return deck.size();
     }
 
     private void generateDeckHelper(String suite){
--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.ArrayList;
+
 class MyTest {
 
 	@Test
@@ -21,5 +23,37 @@ class MyTest {
 		dealer.generateDeck();
 		assertEquals(52,dealer.deck.size());
 	}
+	@Test
+	void shuffleDeck(){
+		BaccaratDealer dealer = new BaccaratDealer();
+		dealer.shuffleDeck();
+		assertEquals(52,dealer.deck.size());
+	}
+	@Test
+	void deckSize(){
+		BaccaratDealer dealer = new BaccaratDealer();
+		dealer.shuffleDeck();
+		assertEquals(52,dealer.deck.size());
+	}
+	@Test
+	void drawOne(){
+		BaccaratDealer dealer = new BaccaratDealer();
+		dealer.generateDeck();
+		dealer.shuffleDeck();
 
+		ArrayList<Card> c = new ArrayList<>();
+		c.add(dealer.drawOne());
+		assertEquals(51,dealer.deck.size());
+		assertEquals(1,c.size());
+	}
+	@Test
+	void dealHand(){
+		BaccaratDealer dealer = new BaccaratDealer();
+		dealer.generateDeck();
+		dealer.shuffleDeck();
+
+		ArrayList<Card> c = dealer.dealHand();
+		assertEquals(50,dealer.deck.size());
+		assertEquals(2,c.size());
+	}
 }
